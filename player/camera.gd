c820a46af2216ddf635fdfb502f96e48aeb5e0d0
breydenlandberg <breydenlandberg@outlook.com>
2026-04-20 19:37:13 +1000

    Added SpringArm3Ds for camera and added tweening to swap (shoulder) alignment

--- player/camera.gd
+++ player/camera.gd
@@ -7,6 +7,15 @@ var max_y_rotation := 1.0
 var mouse_captured := true
 var mouse_sensitivity := 0.001
 
+var camera_tween: Tween
+
+# @export
+@export var camera_alignment_speed := 0.1 # Why does it get slower the higher the number?
+
+# @onready
+@onready var edge_spring_arm := $EdgeSpringArm
+@onready var default_edge_sprint_arm_length: float = edge_spring_arm.spring_length
+
 
 ### fn
 
@@ -24,6 +33,9 @@ func _unhandled_input(event: InputEvent):
 	if mouse_captured and event is InputEventMouseMotion:
 		var mouse_event: Vector2 = event.screen_relative * mouse_sensitivity
 		camera_look(mouse_event)
+
+	if event.is_action_pressed('swap_camera_alignment'):
+		swap_camera_alignment()
 
 
 ## helper
@@ -45,3 +57,15 @@ func capture_mouse():
 func release_mouse():
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	mouse_captured = false
+
+func set_rear_spring_arm_position(pos: float, speed: float):
+	if camera_tween:
+		camera_tween.kill()
+
+	camera_tween = get_tree().create_tween()
+	camera_tween.tween_property(edge_spring_arm, 'spring_length', pos, speed)
+
+func swap_camera_alignment():
+	default_edge_sprint_arm_length = -default_edge_sprint_arm_length
+
+	set_rear_spring_arm_position(default_edge_sprint_arm_length, camera_alignment_speed)
