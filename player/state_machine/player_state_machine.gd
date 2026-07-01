aba97cdd9ffa2536652b0df9718b7f63bca1932c
breydenlandberg <breydenlandberg@outlook.com>
2026-07-01 15:35:16 +1000

    Start work between Unarmed and Armed State Machines

--- player/state_machine/player_state_machine.gd
+++ player/state_machine/player_state_machine.gd
@@ -15,8 +15,8 @@ func _process(_delta):
 	handle_process(_delta)
 
 func _ready():
-	for child: PlayerMotionState in get_children():
-		child._animation_state_changed.connect(animated_model.on_state_machine_animation_state_changed)
-		child._rotate_model.connect(animated_model.on_input_direction_changed)
+	for child: State in get_children():
+		#child._animation_state_changed.connect(animated_model.on_state_machine_animation_state_changed)
+		#child._rotate_model.connect(animated_model.on_input_direction_changed)
 
-	set_up_state_machine()
+		set_up_state_machine()
---
+++ player/state_machine/unarmed_state_machine.gd
@@ -0,0 +1,22 @@
+class_name UnarmedStateMachine extends StateMachine
+
+
+### fn
+
+## virtual
+#
+func _input(_event: InputEvent):
+	handle_input(_event)
+
+func _physics_process(_delta):
+	handle_physics_process(_delta)
+
+func _process(_delta):
+	handle_process(_delta)
+
+func _ready():
+	for child: PlayerMotionState in get_children():
+		child._animation_state_changed.connect(animated_model.on_state_machine_animation_state_changed)
+		child._rotate_model.connect(animated_model.on_input_direction_changed)
+
+	set_up_state_machine()
