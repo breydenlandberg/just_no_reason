8eece06abd087f29ab4bebd1748b61a5fc16f2b1
breydenlandberg <breydenlandberg@outlook.com>
2026-08-05 14:51:00 +1000

    First steps towards finally implementing raycasting and bullet RigidBodyProjectile when shooting

---
+++ player/weapon_manager/projectiles/projectile.gd
@@ -0,0 +1,38 @@
+class_name Projectile extends Node3D
+
+
+### fn
+
+## virtual
+#
+func _set_weapon_projectile(_weapon: Weapon, _model: WeaponModel):
+	pass
+
+
+## helper
+#
+func camera_ray_cast(_range := 100) -> Vector3:
+	var viewport_size: Vector2i
+	var window: Window = get_window()
+
+	match window.content_scale_mode:
+		window.CONTENT_SCALE_MODE_VIEWPORT, window.CONTENT_SCALE_MODE_CANVAS_ITEMS:
+			viewport_size = window.content_scale_size
+		window.CONTENT_SCALE_MODE_DISABLED:
+			viewport_size = window.get_size()
+
+	var camera: Camera3D = get_viewport().get_camera_3d()
+
+	var ray_origin: Vector3 = camera.project_ray_origin(viewport_size / 2.0)
+	var ray_end: Vector3 = ray_origin + camera.project_ray_normal(viewport_size / 2.0) * _range
+
+	var new_ray_query: PhysicsRayQueryParameters3D = PhysicsRayQueryParameters3D.create(ray_origin, ray_end)
+	new_ray_query.set_hit_from_inside(false) # true?
+
+	var intersection: Dictionary = get_world_3d().direct_space_state.intersect_ray(new_ray_query)
+
+	if not intersection.is_empty():
+		var collision: Vector3 = intersection.position
+		return collision
+	else:
+		return ray_end
---
+++ player/weapon_manager/projectiles/rigid_body_projectile.gd
@@ -0,0 +1,29 @@
+class_name RigidBodyProjectile extends Projectile
+
+
+# var
+@export var project_velocity := 100
+@export var expiry_time := 10
+@export var rigid_body_bullet: PackedScene
+
+
+### fn
+
+## virtual
+#
+func _set_weapon_projectile(_weapon: Weapon, _model: WeaponModel):
+	var camera_collision: Vector3 = camera_ray_cast()
+	launch_rigid_projectile(camera_collision, _model, rigid_body_bullet)
+
+## helper
+#
+func launch_rigid_projectile(point: Vector3, model: WeaponModel, bullet: PackedScene):
+	var projectile: RigidBody3D = bullet.instantiate()
+	projectile.top_level = true
+	projectile.position = model.bullet_point.global_position
+
+	add_child(projectile)
+	projectile.look_at(point)
+
+	var direction: Vector3 = (point - model.bullet_point.global_position).normalized()
+	projectile.set_linear_velocity(direction * projectile.velocity)
